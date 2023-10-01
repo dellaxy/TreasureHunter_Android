7b50ec0e1b663bbd9e40a7ac7405a39156a50bd9
Richard <rici.bernath@gmail.com>
2023-10-01 23:22:50 +0200

    functional menu

--- app/src/main/java/com/example/lovci_pokladov/objects/AnimatedMenu.java
+++ app/src/main/java/com/example/lovci_pokladov/objects/AnimatedMenu.java
@@ -2,20 +2,24 @@ package com.example.lovci_pokladov.objects;
 
 import static com.example.lovci_pokladov.objects.ConstantsCatalog.DEFAULT_MENU_HEIGHT;
 import static com.example.lovci_pokladov.objects.ConstantsCatalog.DEFAULT_MENU_WIDTH;
-import static com.example.lovci_pokladov.objects.ConstantsCatalog.OPEN_MENU_HEIGHT;
+import static com.example.lovci_pokladov.objects.ConstantsCatalog.MENU_PAGES;
 import static com.example.lovci_pokladov.objects.ConstantsCatalog.OPEN_MENU_WIDTH;
 
 import android.animation.AnimatorSet;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.cardview.widget.CardView;
 
 import com.example.lovci_pokladov.R;
@@ -27,6 +31,7 @@ public class AnimatedMenu extends LinearLayout {
     private ImageButton menuButton;
     private AnimatedVectorDrawable openMenuDrawable;
     private AnimatedVectorDrawable closeMenuDrawable;
+    LinearLayout menuLayout;
 
     public AnimatedMenu(Context context) {
         super(context);
@@ -43,11 +48,35 @@ public class AnimatedMenu extends LinearLayout {
 
         cardView = findViewById(R.id.menu_card);
         menuButton = findViewById(R.id.menu_button);
+        menuLayout = findViewById(R.id.menu_layout);
+
+        for (MENU_PAGES page : MENU_PAGES.values()) {
+            AppCompatButton button = new AppCompatButton(getContext());
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    getResources().getDimensionPixelSize(R.dimen.DEFAULT_MENU)
+            );
+            layoutParams.setMargins(0, 0, 0, (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, 15, getResources().getDisplayMetrics()));
+
+            button.setLayoutParams(layoutParams);
+            button.setBackgroundResource(R.drawable.rounded_button);
+            button.getBackground().setColorFilter(getResources().getColor(R.color.secondary_light), PorterDuff.Mode.SRC_ATOP);
+            button.setBackground(button.getBackground());
+
+            button.setText(page.getPageName());
+            button.setOnClickListener(view -> {
+                Intent intent = new Intent(getContext(), page.getActivityClass());
+                getContext().startActivity(intent);
+            });
+
+            menuLayout.addView(button);
+        }
+
         openMenuDrawable = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.menu_icon_opened, getContext().getTheme());
         closeMenuDrawable = (AnimatedVectorDrawable) getResources().getDrawable(R.drawable.menu_icon_closed, getContext().getTheme());
 
         menuButton.setImageDrawable(closeMenuDrawable);
-
         menuButton.setOnClickListener(view -> toggleMenu());
     }
 
@@ -58,50 +87,59 @@ public class AnimatedMenu extends LinearLayout {
         if (isMenuOpen) {
             menuButton.setImageDrawable(openMenuDrawable);
             openMenuDrawable.start();
-            animateCardView(DEFAULT_MENU_WIDTH, DEFAULT_MENU_HEIGHT, true);
+            animateCardView(DEFAULT_MENU_WIDTH, true);
         } else {
             menuButton.setImageDrawable(closeMenuDrawable);
-            animateCardView(OPEN_MENU_WIDTH, OPEN_MENU_HEIGHT, false);
+            animateCardView(OPEN_MENU_WIDTH, false);
             closeMenuDrawable.start();
         }
         isMenuOpen = !isMenuOpen;
     }
 
-    private void animateCardView(int targetWidth, int targetHeight, boolean isClosing) {
+    private void animateCardView(int targetWidth, boolean isClosing) {
         int currentWidth = cardView.getWidth();
         int currentHeight = cardView.getHeight();
+        int targetHeight;
 
-        ValueAnimator widthAnimator = ValueAnimator.ofInt(currentWidth, targetWidth);
-        widthAnimator.setDuration(200);
-        widthAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int animatedValue = (int) animation.getAnimatedValue();
-                ViewGroup.LayoutParams params = cardView.getLayoutParams();
-                params.width = animatedValue;
-                cardView.setLayoutParams(params);
-            }
+        // when the menu is being opened, I want the height to match the content.
+        //But since it's animated I can't use wrap_content, so I measure the height of the content.
+        if (isClosing) {
+            targetHeight = DEFAULT_MENU_HEIGHT;
+        } else {
+            cardView.measure(View.MeasureSpec.makeMeasureSpec(cardView.getWidth(), View.MeasureSpec.EXACTLY),
+                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+            targetHeight = cardView.getMeasuredHeight();
+        }
+
+        ValueAnimator widthAnimator = createAnimator(currentWidth, targetWidth, animation -> {
+            int animatedValue = (int) animation.getAnimatedValue();
+            ViewGroup.LayoutParams params = cardView.getLayoutParams();
+            params.width = animatedValue;
+            cardView.setLayoutParams(params);
         });
 
-        ValueAnimator heightAnimator = ValueAnimator.ofInt(currentHeight, targetHeight);
-        heightAnimator.setDuration(200);
-        heightAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int animatedValue = (int) animation.getAnimatedValue();
-                ViewGroup.LayoutParams params = cardView.getLayoutParams();
-                params.height = animatedValue;
-                cardView.setLayoutParams(params);
-            }
+        ValueAnimator heightAnimator = createAnimator(currentHeight, targetHeight, animation -> {
+            int animatedValue = (int) animation.getAnimatedValue();
+            ViewGroup.LayoutParams params = cardView.getLayoutParams();
+            params.height = animatedValue;
+            cardView.setLayoutParams(params);
         });
 
         AnimatorSet animatorSet = new AnimatorSet();
-        if (isClosing) {
+        if(isClosing) {
             animatorSet.playSequentially(heightAnimator, widthAnimator);
         } else {
             animatorSet.playSequentially(widthAnimator, heightAnimator);
         }
         animatorSet.start();
+    }
+
+    private ValueAnimator createAnimator(int startValue, int endValue, ValueAnimator.AnimatorUpdateListener updateListener) {
+        ValueAnimator animator = ValueAnimator.ofInt(startValue, endValue);
+        animator.addUpdateListener(animation -> {
+            updateListener.onAnimationUpdate(animation);
+        });
+        return animator;
     }
 
 }
