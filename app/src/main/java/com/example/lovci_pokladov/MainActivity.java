d732460d864bbc38403fd3f58b0895b426f31dc6
Richard <rici.bernath@gmail.com>
2023-10-05 22:53:10 +0200

    CustomUtils, Fragments optimization

--- app/src/main/java/com/example/lovci_pokladov/MainActivity.java
+++ app/src/main/java/com/example/lovci_pokladov/MainActivity.java
@@ -8,35 +8,49 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.lovci_pokladov.fragments.GameFragment;
+import com.example.lovci_pokladov.fragments.MapsFragment;
+import com.example.lovci_pokladov.objects.Utils;
 import com.example.lovci_pokladov.service_interfaces.MenuClickListener;
 
 public class MainActivity extends AppCompatActivity implements MenuClickListener {
+    private FragmentManager fragmentManager;
+    private Fragment currentFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public void onMenuItemClick(Fragment newFragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        String fragmentTag = newFragment.getClass().getSimpleName();
-        Fragment existingFragment = fragmentManager.findFragmentByTag(fragmentTag);
-
-        if (existingFragment == null) {
-            transaction.replace(R.id.fragment_container, newFragment, fragmentTag);
-        } else {
-            transaction.replace(R.id.fragment_container, existingFragment, fragmentTag);
-        }
-
-        transaction.addToBackStack(null)
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, new MapsFragment(), "MapsFragment")
+                .addToBackStack(null)
                 .commit();
     }
 
     @Override
+    public void onMenuItemClick(Fragment selectedFragment) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        String fragmentTag = selectedFragment.getClass().getSimpleName();
+        Fragment existingFragment = fragmentManager.findFragmentByTag(fragmentTag);
+        currentFragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (!Utils.isNotNull(currentFragment) || !currentFragment.getTag().equals(fragmentTag)) {
+            if (Utils.isNotNull(existingFragment)) {
+                transaction.replace(R.id.fragment_container, existingFragment, fragmentTag);
+            } else {
+                transaction.replace(R.id.fragment_container, selectedFragment, fragmentTag);
+            }
+            transaction.addToBackStack(null)
+                .commit();
+        }
+    }
+
+
+
+    @Override
     public void onBackPressed() {
-        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
 
         if (currentFragment instanceof GameFragment) {
             showExitConfirmationDialog();
--- app/src/main/java/com/example/lovci_pokladov/fragments/MapsFragment.java
+++ app/src/main/java/com/example/lovci_pokladov/fragments/MapsFragment.java
@@ -30,6 +30,7 @@ import com.example.lovci_pokladov.R;
 import com.example.lovci_pokladov.models.LocationMarker;
 import com.example.lovci_pokladov.objects.DatabaseHelper;
 import com.example.lovci_pokladov.objects.GeoJSONLoader;
+import com.example.lovci_pokladov.objects.Utils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -148,7 +149,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
             } else {
                 markers = allMarkers;
             }
-        if (markers != null && !markers.isEmpty()) {
+        if (Utils.isNotEmpty(markers)) {
             for (LocationMarker marker : markers){
                 addMarker(marker);
             }
