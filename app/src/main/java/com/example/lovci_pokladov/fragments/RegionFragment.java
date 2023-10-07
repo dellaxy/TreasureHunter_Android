de52ae438931f5c381168b7943566ea32c40df11
Richard <rici.bernath@gmail.com>
2023-10-07 09:05:58 +0200

    Darkmode cards color bug fixed

--- app/src/main/java/com/example/lovci_pokladov/fragments/RegionFragment.java
+++ app/src/main/java/com/example/lovci_pokladov/fragments/RegionFragment.java
@@ -12,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -70,7 +69,6 @@ public class RegionFragment extends Fragment implements OnMapReadyCallback {
     private void getSelectedRegion(){
         SharedPreferences preferences = requireActivity().getSharedPreferences("MapPreferences", MODE_PRIVATE);
         int selectedRegion = preferences.getInt("selectedRegion", -1);
-        Toast.makeText(requireContext(), "Selected region: " + selectedRegion, Toast.LENGTH_SHORT).show();
         if (selectedRegion != -1) {
             Polygon selectedPolygon = regionsPolygonMap.get(selectedRegion);
             if (selectedPolygon != null) {
