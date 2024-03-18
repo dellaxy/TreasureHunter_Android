348912212b605a89f6eaead30dee87d656ddbcbc
Richard <rici.bernath@gmail.com>
2024-03-18 20:25:56 +0100

    yes

--- app/src/main/java/com/example/city_tours/components/CheckpointTextCard.java
+++ app/src/main/java/com/example/city_tours/components/CheckpointTextCard.java
@@ -8,27 +8,18 @@ import androidx.cardview.widget.CardView;
 
 import com.example.city_tours.R;
 
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-
 public class CheckpointTextCard extends CardView {
-    private TextView checkpointText, checkpointTime;
+    private TextView checkpointText;
 
     public CheckpointTextCard(@NonNull Context context, String text) {
         super(context);
         inflate(getContext(), R.layout.layout_checkpoint_card, this);
         initViews();
 
-        LocalTime currentTime = LocalTime.now(ZoneId.systemDefault());
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        String formattedTime = currentTime.format(formatter);
         checkpointText.setText(text);
-        checkpointTime.setText(formattedTime);
     }
 
     private void initViews() {
         checkpointText = findViewById(R.id.checkpointDescription);
-        checkpointTime = findViewById(R.id.checkpointTime);
     }
 }
